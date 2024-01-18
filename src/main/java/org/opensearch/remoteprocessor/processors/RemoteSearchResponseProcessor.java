874d9df7822de9084ce81008a91d6fe89589b142
HenryL27 <hmlindeman@yahoo.com>
2024-01-18 14:11:56 -0800

    begin processor implementation agnostically to net protocol
    Signed-off-by: HenryL27 <hmlindeman@yahoo.com>

---
+++ src/main/java/org/opensearch/remoteprocessor/processors/RemoteSearchResponseProcessor.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2024 Aryn
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opensearch.remoteprocessor.processors;
+
+import java.util.Map;
+
+import org.opensearch.action.search.SearchRequest;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.http.HttpRequest;
+import org.opensearch.http.HttpResponse;
+import org.opensearch.search.pipeline.AbstractProcessor;
+import org.opensearch.search.pipeline.PipelineProcessingContext;
+import org.opensearch.search.pipeline.Processor;
+import org.opensearch.search.pipeline.SearchResponseProcessor;
+
+public class RemoteSearchResponseProcessor extends AbstractProcessor implements SearchResponseProcessor {
+    public static final String TYPE = "remote_processor";
+    public static final String ENDPOINT_FIELD = "endpoint";
+
+    private final String endpoint;
+
+    protected RemoteSearchResponseProcessor(String tag, String description, boolean ignoreFailure, String endpoint) {
+        super(tag, description, ignoreFailure);
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public SearchResponse processResponse(SearchRequest request, SearchResponse response) throws Exception {
+        throw new UnsupportedOperationException("Use processResponseAsync instead since this processor makes network calls");
+    }
+
+    @Override 
+    public void processResponseAsync(SearchRequest request, SearchResponse response, PipelineProcessingContext ctx, ActionListener<SearchResponse> listener) {
+        // Serialize the request, response, context, and then send it to enpoint.
+        // Wait for response, deserialize it, and return it.
+        HttpRequest httprequest = buildRequest(request, response, ctx);
+        sendRequest(httprequest, ActionListener.wrap(
+            httpresponse -> {
+                listener.onResponse(parseResponse(httpresponse));
+            },
+            e -> {
+                listener.onFailure(e);
+            }
+        ));
+
+    }
+
+    private HttpRequest buildRequest(SearchRequest request, SearchResponse response, PipelineProcessingContext ctx) {
+        throw new UnsupportedOperationException("Not yet implpemented");
+    }
+
+    private void sendRequest(HttpRequest request, ActionListener<HttpResponse> listener) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private SearchResponse parseResponse(HttpResponse response) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static class Factory implements Processor.Factory<SearchResponseProcessor> {
+
+        @Override
+        public SearchResponseProcessor create(
+                Map<String, org.opensearch.search.pipeline.Processor.Factory<SearchResponseProcessor>> processorFactories,
+                String tag, String description, boolean ignoreFailure, Map<String, Object> config,
+                PipelineContext pipelineContext) throws Exception {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'create'");
+        }
+
+    }
+    
+}
